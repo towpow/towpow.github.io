5b155e6d64cf0b426aa7fe6933459f4446267e5d
emiwes <emil.westin1@gmail.com>
2016-12-19 15:28:10 +0100

    Add responsibilities to team members.
    Added an extra paragraph under Challenges

--- functions.js
+++ functions.js
@@ -2,32 +2,38 @@ var team = [
 	{
 		name: "Rickard Bergeling",
 		photo: "img/members/Rickard.png",
-		github: "herrr"
-	}, 
+		github: "herrr",
+		responsibilites: ["3D models", "Networking"]
+	},
 	{
 		name: "Erik Forsberg",
 		photo: "img/members/Erik.jpg",
-		github: "ankhuve"
-	}, 
+		github: "ankhuve",
+		responsibilites: ["Arrow effects", "World modeling"]
+	},
 	{
-		name: "Arvid Sätterkvist", 
+		name: "Arvid Sätterkvist",
 		photo: "img/members/Arvid.png",
-		github: "arvidsat"
-	}, 
+		github: "arvidsat",
+		responsibilites: ["Shaders", "3D models", "Networking"]
+	},
 	{
-		name: "Calle Sténson", 
+		name: "Calle Sténson",
 		photo: "img/members/Calle.jpg",
-		github: "cstensonpv"
-	}, 
+		github: "cstensonpv",
+		responsibilites: ["Enemy logic", "Game logic", "PixelSense interaction"]
+	},
 	{
-		name: "Anton Sivertsson", 
+		name: "Anton Sivertsson",
 		photo: "img/members/Anton.jpg",
-		github: "antonsivertsson"
-	}, 
+		github: "antonsivertsson",
+		responsibilites: ["Bow interaction"]
+	},
 	{
 		name: "Emil Westin",
 		photo: "img/members/Emil.jpg",
-		github: "emiwes"
+		github: "emiwes",
+		responsibilites: ["Networking", "PixelSense interaction", "Bow interaction"]
 	}
 ];
 
@@ -39,10 +45,17 @@ var generateTeamContent = function(){
 		innerHTML += '<img src="'+team[member].photo+'"/>';
 		innerHTML += '<div class="fname">'+team[member].name.split(" ")[0]+'</div>';
 		innerHTML += '<div class="lname">'+team[member].name.split(" ")[1]+'</div>';
-		innerHTML += "</a>";
+		innerHTML += '</a>';
+
+		innerHTML += '<div style="text-align:left;"><span style="color:#159957">Responsible for:</span><ul style="padding-left:15px">';
+		for(var responsibility in team[member].responsibilites){
+			innerHTML += '<li>' + team[member].responsibilites[responsibility] + '</li>';
+		}
+		innerHTML += '</ul></div>';
+
 		innerHTML += '</div>';
 	}
 	document.getElementById("theTeam").innerHTML = innerHTML;
-}
+};
 
 generateTeamContent();
